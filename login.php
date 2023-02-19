693ba576858f78e1cdf92453b7f431faa2f46fa1
John Mejia <johnmejiaduran@gmail.com>
2023-02-19 15:39:07 -0500

    first commit

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+    <link rel="stylesheet" href="http://localhost/ATSES/assets/css/main.css">
+</head>
+<body>
+    <section id="screen-login">
+        <div class="logo-login">
+            <img src="http://localhost/ATSES/images/logo2.png" alt="logo" >
+        </div>
+        <div class="container-login">
+            <form action="" method="" class="form-login">
+                <h1 class="login_title">Bienvenido</h1>
+                <div class="login-group">
+                    <label class="login-label" for="user">Usuario</label>
+                    <input class="login-input" type="text" id="user" name="user" placeholder="">
+                </div>
+                <div class="login-group">
+                    <label class="login-label" for="password">Contraseña</label>
+                    <input class="login-input" type="password" name="password" id="password" placeholder=" ">
+                </div>
+                <p class="login-parragraph"><a href="#" class="login-link">¿Olvidaste tu Contraseña?</a></p>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
