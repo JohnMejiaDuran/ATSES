e9151f72c9c7d72dadfa5263d4702c31a5956474
JohnMejia <johnmejiaduran@gmail.com>
2023-02-22 16:53:57 -0500

    Changed slidebar, login overflow and border main

--- layout/aside.php
+++ layout/aside.php
@@ -2,48 +2,48 @@
     <aside class="barra-nav"> 
             <ul>
         
-                <li><a class="padre" href="#">Gestionar Datos</a> <!--padre-->
+                <li><a class="nav-father" href="#">Gestionar Datos</a> <!--padre-->
                     <ul>
-                        <li><a href="#">Cliente</a> <!--hijo-->
+                        <li><a class="nav-son" href="#">Cliente</a> <!--hijo-->
                             <ul>
-                                <li><a href="http://localhost/ATSES/registroCliente.php">Registrar</a></li> <!--nieto-->
-                                <li><a href="http://localhost/ATSES/actualizarCliente.php">Actualizar</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" class="nav-href="http://localhost/ATSES/registroCliente.php">Registrar</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" href="http://localhost/ATSES/actualizarCliente.php">Actualizar</a></li> <!--nieto-->
                             </ul>
                         </li>
-                        <li><a href="#">Proveedor</a> <!--hijo-->
+                        <li><a class="nav-son" href="#">Proveedor</a> <!--hijo-->
                             <ul>
-                                <li><a href="/RegistroProveedor.html">Registrar</a></li> <!--nieto-->
-                                <li><a href="/ActualizarProveedor.html">Actualizar</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" href="/RegistroProveedor.html">Registrar</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" href="/ActualizarProveedor.html">Actualizar</a></li> <!--nieto-->
                             </ul>
                         </li>
-                        <li><a href="#">Administrador</a><!--hijo-->
+                        <li><a  class="nav-son" href="#">Administrador</a><!--hijo-->
                             <ul>
-                                <li><a href="/RegistroAdministrador.html">Registrar</a></li> <!--nieto-->
-                                <li><a href="/ActualizarAdministrador.html">Actualizar</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" href="/RegistroAdministrador.html">Registrar</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" href="/ActualizarAdministrador.html">Actualizar</a></li> <!--nieto-->
                             </ul>
                         </li>
                     </ul>
                 </li>   
-                <li><a  class="padre" href="#">Gestionar Estructuras</a><!--padre-->
+                <li><a  class="nav-father" href="#">Gestionar Estructuras</a><!--padre-->
                     <ul>
-                        <li><a href="#">Estructuras</a> <!--Hijo-->
+                        <li><a class="nav-son" href="#">Estructuras</a> <!--Hijo-->
                             <ul>
-                                <li><a href="/RegistroEstructura.html">Registrar Estructura</a></li> <!--nieto-->
-                                <li><a href="/ActualizarEstrctura.html">Actualizar Estructura</a></li><!--nieto-->
+                                <li><a class="nav-grandson" href="/RegistroEstructura.html">Registrar Estructura</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" href="/ActualizarEstrctura.html">Actualizar Estructura</a></li><!--nieto-->
                             </ul>
                         </li>
-                        <li><a href="#">Materiales</a> <!--Hijo-->
+                        <li><a class="nav-son" href="#">Materiales</a> <!--Hijo-->
                             <ul>
-                                <li><a href="/RegistroMateriales.html">Registrar Material</a></li> <!--nieto-->
-                                <li><a href="/ActualizarMateriales.html">Actualizar Material</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" href="/RegistroMateriales.html">Registrar Material</a></li> <!--nieto-->
+                                <li><a class="nav-grandson" href="/ActualizarMateriales.html">Actualizar Material</a></li> <!--nieto-->
                             </ul>
                         </li>
                     </ul>
                 </li>         
-                <li><a class="padre" href="#">Gestionar Cotizaciones</a> <!--padre-->
+                <li><a class="nav-father" href="#">Gestionar Cotizaciones</a> <!--padre-->
                     <ul>
-                        <li><a href="/CrearCotización.html">Crear Cotización</a></li> <!--Hijo-->
-                        <li><a href="/ImprimirCotización.html">Imprimir Cotización</a></li> <!--hijo-->
+                        <li><a class="nav-son" href="/CrearCotización.html">Crear Cotización</a></li> <!--Hijo-->
+                        <li><a class="nav-son" href="/ImprimirCotización.html">Imprimir Cotización</a></li> <!--hijo-->
                         
                     </ul>
                 </li>
