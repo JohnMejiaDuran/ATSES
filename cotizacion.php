f776be9e2d24ae187fb65195542dc900f7ea52e4
JohnMejia <johnmejiaduran@gmail.com>
2023-03-03 17:38:27 -0500

    Quote form

--- cotizacion.php
+++ cotizacion.php
@@ -4,7 +4,29 @@
 
     <main>
         <section>
-            <thead>Cotizacion</thead>
+            <table>
+                <thead>
+                    <tr>
+                        <th><h1>Cotizacion</h1></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td><input type="number" placeholder="Cedula o Nit"></td>
+                        <td><input type="text" placeholder="Nombre o razon social"></td>
+                        <td><input type="number" placeholder="Telefono"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" placeholder="Dirección"></td>
+                        <td><input type="email" placeholder="Email"></td>
+                    </tr>
+                    <tr>
+                        <td><h2>Datos de cotizacion:</h2></td>
+                    </tr>
+                </tbody>
+            
+            </table>
         </section>
     </main>
 
