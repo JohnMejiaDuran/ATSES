b15804613344bc96d922a471e8a40e71705bc828
John Mejia <johnmejiaduran@gmail.com>
2023-03-24 03:17:56 -0500

    export to css grid

--- layout/footer.php
+++ layout/footer.php
@@ -1,9 +1,9 @@
 
-        <footer>
-            <div class="text-footer">
-                <p>ATSES soluciones estructurales</p>
+        <footer class="footer">
+            <div class="footer__container">
+                <p class="footer__text">ATSES soluciones estructurales</p>
             </div>
         </footer>
-
+    </div>
 </body>
 </html>
--- layout/header.php
+++ layout/header.php
@@ -5,17 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atses soluciones estructurales</title>
+    <script src="https://kit.fontawesome.com/ad3f926aa1.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="http://localhost/ATSES/assets/css/main.css">
 </head>
 <body >
-    <header>
-        <a href="http://localhost/ATSES/index.php" class="logo"> 
-            <img src="http://localhost/ATSES/images/logo2.PNG" alt="">
-        </a>
-        <a href="" class="img-perfil">
-            <p>Bienvenido <span>Mario</span></p>
-            <img for="perfil" src="http://localhost/ATSES/images/admi.png" alt="imagen de perfil">
-        </a>
-    </header>
+    <div class="container">
+        <header class="header">
+            
+            <a href="" class="header__perfil">
+                <p class="header__text-welcome">Bienvenido&nbsp<span class="header__admin">Mario</span></p>
+                <img for="perfil" src="http://localhost/ATSES/images/admi.png" alt="imagen de perfil" class="header__img-perfil">
+            </a>
+        </header>
         
+    
