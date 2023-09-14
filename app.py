e43104d8efa77fe470c851fe957241dd16931450
Marly Arrieta <delany0329@gmail.com>
2023-09-14 12:54:06 -0500

    .

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from config import Config
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/atsesdb"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://Marly:sofia120329@localhost/atsesdb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config.from_object(Config)
 app.register_blueprint(cliente)
