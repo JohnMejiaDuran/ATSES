838ad11a46590f026a10e8f5d7b6cfa75d6a427e
Carlos Ovidio <carlosovidiogarciamenco@gmail.com>
2023-09-14 17:25:51 -0500

    Bd materiales

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from routes.clientes import cliente
 from routes.home import home
 from routes.proveedores import proveedor
 from config import Config
+from routes.materiales import material
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/atsesdb"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://Carlos:Futbol@localhost/atsesdb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config.from_object(Config)
 app.register_blueprint(cliente)
 app.register_blueprint(home)
 app.register_blueprint(proveedor)
+app.register_blueprint(material)
